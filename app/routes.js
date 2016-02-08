985f1a74c81264dd1688f63dd538e0d860a5a762
Chinedum <cukejianya@gmail.com>
2016-02-08 12:45:21 -0500

    Set up routes

---
+++ app/routes.js
@@ -0,0 +1,9 @@
+var routes = function(app, passport) {
+  app.get('/', function(req, res) {
+    res.render('index.ejs');
+  });
+
+  app.get('/login', function(req,res) {
+    res.render('login.ejs', {message: req.flash('loginMessage') });
+  });
+}
