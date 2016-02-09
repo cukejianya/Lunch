5fc8fe20d6406616fc9fef83c8c932b52b8f24e5
Chinedum <cukejianya@gmail.com>
2016-02-08 19:19:40 -0500

    Add passport authentication

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,12 @@ var routes = function(app, passport) {
     res.render('login.ejs', {message: req.flash('loginMessage') });
   });
 
-  //*process login stuff HERE*
+  //*process login stuff HERE
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/profile',
+    failureRedirect : '/login',
+    failureFlash : true
+  }))
 
   app.get('/signup', function(req, res) {
     res.render('signup.ejs', {message: req.flash('signupMessage') });
