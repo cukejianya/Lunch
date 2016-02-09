a2fb28811696fc45501e7f4564651563469cba23
Chinedum <cukejianya@gmail.com>
2016-02-08 19:15:35 -0500

    Finish passport config

--- app/models/user.js
+++ app/models/user.js
@@ -7,3 +7,13 @@ var userSchema = mongoose.Schema({
     password: String,
   },
 })
+
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
